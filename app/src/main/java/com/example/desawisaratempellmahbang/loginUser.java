17c0854914c0c6ee85e3f0a4e85e09375f4fe00e
upinreborn <alfinhilmy6@gmail.com>
2021-10-25 14:05:53 +0700

    Commit

--- app/src/main/java/com/example/desawisaratempellmahbang/loginUser.java
+++ app/src/main/java/com/example/desawisaratempellmahbang/loginUser.java
@@ -2,31 +2,69 @@ package com.example.desawisaratempellmahbang;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.method.LinkMovementMethod;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
 public class loginUser extends AppCompatActivity {
 
     TextView btndaftar;
+    EditText ePassword, eUsername;
+    Button btnlogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_user);
 
-        TextView btndaftar =  findViewById(R.id.btndaftar);
+        TextView btndaftar =  findViewById(R.id.btnSignupInLogin);
+        Button btnlogin = findViewById(R.id.btnlogin);
+        EditText ePassword = findViewById(R.id.ePassword);
+        EditText eUsername = findViewById(R.id.eUsername);
+
         btndaftar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent daftar = new Intent(loginUser.this, registerUser.class);
                 startActivity(daftar);
                 finish();
+            }
+        });
+
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences mPreferences;
+                String mSharedPrefFile =
+                        "com.example.desawisaratempellmahbang";
+
+                mPreferences=getSharedPreferences(mSharedPrefFile,
+                                Activity.MODE_PRIVATE);
+
+                String uname = mPreferences.getString("Username",null);
+
+                String pwd = mPreferences.getString("Password",null);
+
+                if (uname.equals(eUsername.getText().toString()) && pwd.equals(ePassword.getText().toString())){
+                    Intent intent = new Intent(loginUser.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+                else if(eUsername.getText().toString().equals("") || ePassword.getText().toString().equals("")){
+                    Toast.makeText(getApplicationContext(), "username and password must be filled", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "Wrong username or Password", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
--- app/src/main/java/com/example/desawisaratempellmahbang/registerUser.java
+++ app/src/main/java/com/example/desawisaratempellmahbang/registerUser.java
@@ -2,23 +2,38 @@ package com.example.desawisaratempellmahbang;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
+import android.widget.Toast;
 
 public class registerUser extends AppCompatActivity {
 
     TextView btnlogin;
+    Button btnSignup;
+    EditText eUsername, eFullname, ePassword;
+    public SharedPreferences mPreferences;
+    public static final String mSharedPrefFile =
+            "com.example.desawisaratempellmahbang";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_user);
 
-        TextView btnlogin = findViewById(R.id.btnlogin);
+        TextView btnlogin = findViewById(R.id.btnloginInRegister);
+        EditText eUsername = findViewById(R.id.eUsername);
+        EditText ePassword = findViewById(R.id.ePassword);
+        EditText eFullname = findViewById(R.id.eFullname);
+        Button btnSignup = findViewById(R.id.btnsignup);
+
         btnlogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -28,6 +43,40 @@ public class registerUser extends AppCompatActivity {
             }
         });
 
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mPreferences=getSharedPreferences(mSharedPrefFile,
+                        Activity.MODE_PRIVATE);
+                SharedPreferences.Editor editor=mPreferences.edit();
 
+                editor.putString("Username",eUsername.getText().toString());
+                editor.putString("Password",ePassword.getText().toString());
+                editor.putString("Fullname",eFullname.getText().toString());
+                editor.commit();
+                pesan();
+            }
+
+            private void pesan()
+            {
+                AlertDialog.Builder psn=new
+                        AlertDialog.Builder(registerUser.this);
+                psn.setTitle("Konfirmasi");
+                psn.setMessage("Data Tersimpan");
+                psn.setIcon(R.drawable.ic_launcher_background);
+                psn.setNeutralButton("OK",new
+                        DialogInterface.OnClickListener()
+                        {
+                            @Override
+                            public void onClick(DialogInterface
+                                                        dialogInterface, int i) {
+
+                                Toast.makeText(getApplicationContext(),"Sukses simpan",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                psn.show();
+            }
+        });
     }
 }
