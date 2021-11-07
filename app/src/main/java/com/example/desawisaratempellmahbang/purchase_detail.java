a4a7c75c00fbe727c97f88a6559579d2db0f01b1
LENOVO <alfinhilmy6@gmail.com>
2021-11-07 10:15:18 +0700

    update input pembayaran

--- app/src/main/java/com/example/desawisaratempellmahbang/purchase_detail.java
+++ app/src/main/java/com/example/desawisaratempellmahbang/purchase_detail.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class purchase_detail extends AppCompatActivity {
@@ -17,5 +20,36 @@ public class purchase_detail extends AppCompatActivity {
         String purchaseAmount = intent.getStringExtra("purchaseAmount");
         TextView purchaseAmountTv = findViewById(R.id.totalpayment);
         purchaseAmountTv.setText(purchaseAmount);
+
+        EditText edtPurchaseDetail = findViewById(R.id.jumlahdibayar);
+        edtPurchaseDetail.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                changePaymentAmount();
+            }
+        });
     }
-}
+
+    public void changePaymentAmount() {
+        Intent intent = getIntent();
+        Integer purchaseAmount = Integer.parseInt(intent.getStringExtra("purchaseAmount")) ;
+        EditText edtPaymentAmount = findViewById(R.id.jumlahdibayar);
+        TextView returnAmountText = findViewById(R.id.charge);
+        if(edtPaymentAmount.getText().toString().equals("") || edtPaymentAmount.getText().toString().equals("0")) {
+            return;
+        }
+        Integer paymentAmount = Integer.parseInt(edtPaymentAmount.getText().toString());
+        Integer returnAmount = paymentAmount - purchaseAmount;
+        returnAmountText.setText(returnAmount.toString());
+    }
+};
